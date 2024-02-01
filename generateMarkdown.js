d145b9a44aea71421e0a655215712a963a8c5129
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-02-01 17:59:20 -0600

    minor changes to function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,16 +2,16 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
     switch (license) {
-      case 'MIT':
-        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      case `MIT`:
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
       case 'BSD 3':
         return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
-      case 'Apache 2.0':
-        return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-      case 'GPL 3.0':
-        return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      case `Apache 2.0`:
+        return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      case `GPL 3.0`:
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
         
-      case 'None':
+      case `None`:
         return '';
     }
   }
