d3f313e03f151674610327b57ae580f91277c5f9
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-02-01 18:28:20 -0600

    minor chages to licenseLink function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -18,7 +18,18 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    switch (license) {
+      case `MIT`:
+        return `[MIT](https://opensource.org/licenses/MIT)`;
+      case `BSD 3`:
+        return `[BSD 3](https://opensource.org/licenses/BSD-3-Clause)`;
+      case `Apache 2.0`:
+        return `[Apache 2.0](https://opensource.org/licenses/Apache-2.0)`;
+      case `GPL 3.0`:
+        return `[GPL 3.0](https://www.gnu.org/licenses/gpl-3.0)`;
+    }
+  }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
